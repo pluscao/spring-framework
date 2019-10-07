dd1a0a1abc8534e0c7f04a953c080ca09981c07a
silveryC <hello.shengC@gmail.com>
2019-10-07 15:10:29 +0800

    debug module build success!

--- spring-gradle-debug/src/main/java/TestMain.java
+++ spring-gradle-debug/src/main/java/TestMain.java
@@ -1,7 +1,7 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test {
+public class TestMain {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-config.xml");
 	}
